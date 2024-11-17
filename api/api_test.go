e51262995404d883fdbe5c03b27d720711ac3182
Mia O. Appel <mail@maxap.eu>
2024-11-17 14:22:46 +0100

    Added framework for table tests

--- api/api_test.go
+++ api/api_test.go
@@ -2,10 +2,48 @@ package api_test
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
+
+type wantData struct {
+	body   string
+	status int
+}
+
+type param struct {
+	key   string
+	value string
+}
+type givenData struct {
+	body   string
+	params []param
+	uuid   string
+}
+
+type test struct {
+	name  string
+	input givenData
+	want  wantData
+}
+
+func (dat *givenData) getBody() string {
+	if dat == nil {
+		return ""
+	}
+	return dat.body
+}
+
+func (dat *givenData) getUUID() string {
+	if dat == nil {
+		return ""
+	}
+	return dat.uuid
+}
 
 var apiPath = "/api"
 
@@ -33,115 +71,276 @@ func TestPutGetraenk(t *testing.T) {
 
 	//TODO: Setup backend
 
-	t.Run("minimal input", func(t *testing.T) {
-		w := httptest.NewRecorder()
-		req, _ := http.NewRequest("PUT", apiPath+"/getraenk", nil)
+	//TODO: remove custom definition of types
+	type wantTarget struct {
+		body   string
+		status int
+	}
+	var tests = []struct {
+		name  string
+		input string
+		want  wantTarget
+	}{
+		//TODO: add proper request bodies
+		{"minimal input", "", wantTarget{"", 201}},
+		{"fully populated", "", wantTarget{"", 201}},
+		{"invalid name", "", wantTarget{"", 400}},
+		{"invalid likes", "", wantTarget{"", 400}},
+		{"invalid bics", "", wantTarget{"", 400}},
+	}
 
-		//TODO: generate body, evaluate usage of testing/Fuzzing
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			req, _ := http.NewRequest("PUT", apiPath+"/getraenk", strings.NewReader(tt.input))
+			router.ServeHTTP(w, req)
+			assert.Equal(t, w.Code, tt.want.status)
+			if tt.want.body != "" {
+				assert.Equal(t, w.Body.String(), tt.want.body)
+			}
 
-		router.ServeHTTP(w, req)
-
-		//TODO: assert correctness of request processing
-	})
-
-	t.Run("fully populated input", func(t *testing.T) {
-		w := httptest.NewRecorder()
-		req, _ := http.NewRequest("PUT", apiPath+"/getraenk", nil)
-
-		//TODO: generate body, evaluate usage of testing/Fuzzing
-
-		router.ServeHTTP(w, req)
-
-		//TODO: assert correctness of request processing
-	})
-
-	t.Run("invalid Name", func(t *testing.T) {
-		w := httptest.NewRecorder()
-		req, _ := http.NewRequest("PUT", apiPath+"/getraenk", nil)
-
-		//TODO: generate body, evaluate usage of testing/Fuzzing
-
-		router.ServeHTTP(w, req)
-
-		//TODO: assert correctness of request processing
-	})
-
-	t.Run("invalid likes", func(t *testing.T) {
-		w := httptest.NewRecorder()
-		req, _ := http.NewRequest("PUT", apiPath+"/getraenk", nil)
-
-		//TODO: generate body, evaluate usage of testing/Fuzzing
-
-		router.ServeHTTP(w, req)
-
-		//TODO: assert correctness of request processing
-	})
-
-	t.Run("invalid bics", func(t *testing.T) {
-		w := httptest.NewRecorder()
-		req, _ := http.NewRequest("PUT", apiPath+"/getraenk", nil)
-
-		//TODO: generate body, evaluate usage of testing/Fuzzing
-
-		router.ServeHTTP(w, req)
-
-		//TODO: assert correctness of request processing
-	})
+		})
+	}
 
 }
 
 // TestGetGetraenk runs test on the /getraenk API endpoint using the GET Method
 // correct output after adding a getraenk as well as correct handling of malformed GET requests
 func TestGetGetraenk(t *testing.T) {
+	var tests = []test{
+		{"expected request", givenData{}, wantData{status: 200}},
+		{"random parameters", givenData{"",
+			[]param{
+				{"randomKey1", "randomValue1"},
+				{"adasd2123inj", "23edsad"}}, ""},
+			wantData{status: 200},
+		},
+		{"unexpected body", givenData{body: "here be bodies"}, wantData{status: 200}},
+	}
+
 	router := gin.Default()
 
-	//TODO: tests
+	//TODO: Setup backend
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			var reqBody io.Reader
+			if tt.input.getBody() != "" {
+				reqBody = strings.NewReader(tt.input.getBody())
+			}
+			req, _ := http.NewRequest("GET", apiPath+"/getraenk", reqBody)
+			//add parameters to request
+			if tt.input.params != nil {
+				q := req.URL.Query()
+				for _, param := range tt.input.params {
+					q.Add(param.key, param.value)
+				}
+			}
+			router.ServeHTTP(w, req)
+			assert.Equal(t, w.Code, tt.want.status)
+			if tt.want.body != "" {
+				assert.Equal(t, w.Body.String(), tt.want.body)
+			}
+		})
+	}
+
 }
 
 // TestGetGetraenkByUUID runs tests on the /getraenk/{uuid} endpoint using the GET Method
 // correct object retrieval, error handling and handling of malformed requests is tested
 func TestGetGetraenkByUUID(t *testing.T) {
-	router := gin.Default()
+	var tests = []test{}
+	//TODO: Add tests
 
-	//TODO: tests
+	router := gin.Default()
+	//TODO: Setup backend
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			var reqBody io.Reader
+			if tt.input.getBody() != "" {
+				reqBody = strings.NewReader(tt.input.getBody())
+			}
+			req, _ := http.NewRequest("GET", apiPath+"/getraenk/"+tt.input.getUUID(), reqBody)
+			//add parameters to request
+			if tt.input.params != nil {
+				q := req.URL.Query()
+				for _, param := range tt.input.params {
+					q.Add(param.key, param.value)
+				}
+			}
+			router.ServeHTTP(w, req)
+			assert.Equal(t, w.Code, tt.want.status)
+			if tt.want.body != "" {
+				assert.Equal(t, w.Body.String(), tt.want.body)
+			}
+		})
+	}
 }
 
 // TestPatchGetraenkByUUID runs tests on the /getraenk/{uuid} endpoint using the PATCH Method
 // correct object updating, input validation, error handling and handling of malformed requests is tested
 func TestPatchGetraenkByUUID(t *testing.T) {
-	router := gin.Default()
+	var tests = []test{}
+	//TODO: Add tests
 
-	//TODO: tests
+	router := gin.Default()
+	//TODO: Setup backend
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			var reqBody io.Reader
+			if tt.input.getBody() != "" {
+				reqBody = strings.NewReader(tt.input.getBody())
+			}
+			req, _ := http.NewRequest("PATCH", apiPath+"/getraenk/"+tt.input.getUUID(), reqBody)
+			//add parameters to request
+			if tt.input.params != nil {
+				q := req.URL.Query()
+				for _, param := range tt.input.params {
+					q.Add(param.key, param.value)
+				}
+			}
+			router.ServeHTTP(w, req)
+			assert.Equal(t, w.Code, tt.want.status)
+			if tt.want.body != "" {
+				assert.Equal(t, w.Body.String(), tt.want.body)
+			}
+		})
+	}
 }
 
 // TestDeleteGetraenk runs tests on the /getrank/{uuid} endpoint using the DELETE method
 // correct object deletion, error handling and handling of malformed requests is tested
 func TestDeleteGetraenk(t *testing.T) {
-	router := gin.Default()
+	var tests = []test{}
+	//TODO: Add tests
 
-	//TODO: tests
+	router := gin.Default()
+	//TODO: Setup backend
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			var reqBody io.Reader
+			if tt.input.getBody() != "" {
+				reqBody = strings.NewReader(tt.input.getBody())
+			}
+			req, _ := http.NewRequest("DELETE", apiPath+"/getraenk/"+tt.input.getUUID(), reqBody)
+			//add parameters to request
+			if tt.input.params != nil {
+				q := req.URL.Query()
+				for _, param := range tt.input.params {
+					q.Add(param.key, param.value)
+				}
+			}
+			router.ServeHTTP(w, req)
+			assert.Equal(t, w.Code, tt.want.status)
+			if tt.want.body != "" {
+				assert.Equal(t, w.Body.String(), tt.want.body)
+			}
+		})
+	}
 }
 
 // TestGetraenkDislike runs tests on the /getraenk/{uuid}/dislike endpoint using the POST Method
 // correct object updates, input validation, error handling and handling of malformed requests is tested
 func TestGetraenkDislike(t *testing.T) {
-	router := gin.Default()
+	var tests = []test{}
+	//TODO: Add tests
 
-	//TODO: tests
+	router := gin.Default()
+	//TODO: Setup backend
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			var reqBody io.Reader
+			if tt.input.getBody() != "" {
+				reqBody = strings.NewReader(tt.input.getBody())
+			}
+			req, _ := http.NewRequest("POST", apiPath+"/getraenk/"+tt.input.getUUID()+"/dislike", reqBody)
+			//add parameters to request
+			if tt.input.params != nil {
+				q := req.URL.Query()
+				for _, param := range tt.input.params {
+					q.Add(param.key, param.value)
+				}
+			}
+			router.ServeHTTP(w, req)
+			assert.Equal(t, w.Code, tt.want.status)
+			if tt.want.body != "" {
+				assert.Equal(t, w.Body.String(), tt.want.body)
+			}
+		})
+	}
 }
 
 // TestGetraenklike runs tests on the /getraenk/{uuid}/like endpoint using the POST Method
 // correct object updates, input validation, error handling and handling of malformed requests is tested
 func TestGetraenkLike(t *testing.T) {
-	router := gin.Default()
+	var tests = []test{}
+	//TODO: Add tests
 
-	//TODO: tests
+	router := gin.Default()
+	//TODO: Setup backend
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			var reqBody io.Reader
+			if tt.input.getBody() != "" {
+				reqBody = strings.NewReader(tt.input.getBody())
+			}
+			req, _ := http.NewRequest("POST", apiPath+"/getraenk/"+tt.input.getUUID()+"/like", reqBody)
+			//add parameters to request
+			if tt.input.params != nil {
+				q := req.URL.Query()
+				for _, param := range tt.input.params {
+					q.Add(param.key, param.value)
+				}
+			}
+			router.ServeHTTP(w, req)
+			assert.Equal(t, w.Code, tt.want.status)
+			if tt.want.body != "" {
+				assert.Equal(t, w.Body.String(), tt.want.body)
+			}
+		})
+	}
 }
 
 // TestGetraenkSuperlike runs tests on the /getraenk/{uuid}/superlike endpoint using the POST Method
 // correct object updates, input validation, error handling and handling of malformed requests is tested
 func TestGetraenkSuperlike(t *testing.T) {
-	router := gin.Default()
+	var tests = []test{}
+	//TODO: Add tests
 
-	//TODO: tests
+	router := gin.Default()
+	//TODO: Setup backend
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			var reqBody io.Reader
+			if tt.input.getBody() != "" {
+				reqBody = strings.NewReader(tt.input.getBody())
+			}
+			req, _ := http.NewRequest("POST", apiPath+"/getraenk/"+tt.input.getUUID()+"/superlike", reqBody)
+			//add parameters to request
+			if tt.input.params != nil {
+				q := req.URL.Query()
+				for _, param := range tt.input.params {
+					q.Add(param.key, param.value)
+				}
+			}
+			router.ServeHTTP(w, req)
+			assert.Equal(t, w.Code, tt.want.status)
+			if tt.want.body != "" {
+				assert.Equal(t, w.Body.String(), tt.want.body)
+			}
+		})
+	}
 }
